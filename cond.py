6a295b0b757ee3b90d1014970026d5abf9eddbc5
tasawar110 <126941328+tasawar110@users.noreply.github.com>
2023-03-05 11:00:48 -0800

    Add files via upload

---
+++ cond.py
@@ -0,0 +1,49 @@
+#task
+password='PF23'
+a=input('enter your password:')
+if (a==password):
+    print('You are successfully login.')
+else:
+    print('you have entered incorrect password')    
+#task2
+password='PF23'
+mobile= 923073514050  
+a=input('enter your password:')  
+b=input('enter your mobile num:')  
+if (a==password) and (b==mobile):
+    print('Log in successful')
+else:
+    print('log in failed')  
+#task3
+#Python, Java, C++, Mathematics and DSA  5 subjects grading
+a=int(input('enter your % marks in total:'))
+
+if(a>=90):
+    print('Grade A')
+elif(a>=80):
+    print('Grade B')
+elif(a>=70):
+    print('Grade C')
+elif(a>=60):
+    print('Grade D')
+elif(a>=50):
+    print('Grade E')   
+elif(a>=40):
+    print('Grade F')   
+#task4
+a=input('enter an alphabet:')
+if (a=='a'):
+    print('vowel')  
+elif(a=='i') :
+    print('vowel')
+elif(a=='o') :
+    print('vowel')
+elif(a=='u') :
+    print('vowel')
+elif(a=='e') :
+    print('vowel') 
+else:
+    print('consant')      
+
+             
+
